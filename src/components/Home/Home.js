d2f2351e980ae24561c655eeb0b2604e4f487989
YoRHa <jane0908h@outlook.com>
2026-03-08 15:18:36 +0300

    test

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -30,7 +30,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name">Jessica Lawshe</strong>
+                <strong className="main-name">&nbsp;Dhana Santini</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
