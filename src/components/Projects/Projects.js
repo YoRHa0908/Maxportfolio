ad3d413eb9d2e1d5e2da8734fdd3c74a08cd427e
YoRHa <jane0908h@outlook.com>
2026-03-12 06:33:49 +0300

    projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,7 +6,7 @@ import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
 import NetGuardXAI_nids from "../../Assets/Projects/NetGuardXAI_nids.png";
 import typescript from "../../Assets/Projects/typescript.png";
-import Figma from "../../Assets/Projects/Figma.png";
+import AI_Social from "../../Assets/Projects/AI_Social.png";
 import realtime_chat_app from "../../Assets/Projects/realtime-chat-app.png";
 import Feedback_Widget from "../../Assets/Projects/Feedback_Widget.png";
 
@@ -64,11 +64,11 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={Figma}
+              imgPath={AI_Social}
               isBlog={false}
               title="Ai For Social Good"
               description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              ghLink="https://github.com/FBlinker/AI_For_Social_Good"
             />
           </Col>
 
@@ -79,7 +79,7 @@ function Projects() {
               title="Face Recognition and Emotion Detection"
               description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              ghLink="https://github.com/FBlinker/Face_And_Emotion_Detection"
             />
           </Col>
         </Row>
